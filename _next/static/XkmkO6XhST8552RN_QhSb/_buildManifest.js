84ba0c811134267e27cf67f89d57132f3c6c8dc1
gdem25 <58720487+gdem25@users.noreply.github.com>
2022-09-16 08:18:00 +0000

    Deploying to gh-pages from @ CSUN-LLIU/sd.github.io@0c53b650e27629c313b67e85b96020eb3a44ee2a 🚀

--- _next/static/XkmkO6XhST8552RN_QhSb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-dad737c866d19bf0.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
