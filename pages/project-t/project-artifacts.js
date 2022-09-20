348c9ebcc179606cd23532173d783af7ece9d858
matt dung <35019748+MattoScientisuto@users.noreply.github.com>
2022-09-19 19:17:21 -0700

    Project T Team Members

--- pages/project-t/project-artifacts.js
+++ pages/project-t/project-artifacts.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const ProjectArtifacts = () => {
-  return <div>ProjectArtifacts</div>
+  return <div>
+    <h1>Project Artifacts </h1> 
+    <p>Placeholder</p> 
+  </div>
 }
 
 export default ProjectArtifacts
--- pages/project-t/team-members.js
+++ pages/project-t/team-members.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const TeamMembers = () => {
-  return <div>TeamMembers</div>
+  return <div>
+    <h1>Team Members:</h1> 
+    <p>Charles Cabugnason: Experience with java, js, HTML, CSS. Interest in Front-End and Music Software development.</p>
+    <p>Sari Ajina: Experience in Java, JS, Swift, HTML, CSS. Interested in Full-Stack Engineering and want to gain knowledge in Python Machine Learning. </p>
+    <p> Brent Ilejay: Experience in C++, Java, Python, and Swift (App Development)</p>
+    <p>Matt Duong: Experience in Java, CSS, Some HTML, JavaScript, MongoDB Database </p>
+    <p>Marc Talladen: Experience with Java, HTML, JavaScript. Interested in Front-End and Game Development</p>
+    <p>Antranic Honanian: I have html,css,js,c# and unity experience </p>
+  </div>
 }
 
 export default TeamMembers
