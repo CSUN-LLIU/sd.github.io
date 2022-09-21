2f2bc14d8cbe33cc707d15aeba011f397efb492a
luckytrout <100403421+luckytrout@users.noreply.github.com>
2022-09-21 11:46:44 -0700

    Co-authored-by: Kathleen Navarro <klnavarro@users.noreply.github.com> Co-authored-by: Gino Vitug <gino-vi@users.noreply.github.com> Co-authored-by: Murs <murssal@users.noreply.github.com>

--- pages/bit-by-bit/project-artifacts.js
+++ pages/bit-by-bit/project-artifacts.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
 const ProjectArtifacts = () => {
-  return <div>ProjectArtifacts</div>
+  /*
+  3. Project Artifacts
+    Create placeholders for all development and research documents that your team will create here, for example:
+
+    Prototype Design 
+    Final Presentation
+    Final Project Report
+    Posters
+    Videos
+  */
+ 
+  return <div>Project Artifacts
+    <div>
+      <h2>Prototype Design</h2>
+    </div>
+    <div>
+      <h2>Final Presentation</h2>
+    </div>
+    <div>
+      <h2>Final Project Report</h2>
+    </div>
+    <div>
+     <h2> Posters </h2>
+    </div>
+    <div>
+      <h2>Videos</h2>
+    </div>
+  </div>
 }
 
 export default ProjectArtifacts
