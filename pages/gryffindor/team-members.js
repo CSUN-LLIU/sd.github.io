dc1caeb3db52b9db7400354a0ea689ff19d7df07
sopiloteh <bravoerick9@outlook.com>
2022-09-21 15:14:46 -0700

    update
    minimal

--- pages/gryffindor/team-members.js
+++ pages/gryffindor/team-members.js
@@ -7,32 +7,32 @@ const TeamMembers = () => {
   <h1>Team Gryffindor</h1>
 
     <div class="card bg-light">
-    <p>Jazlyn Fernandez:</p>
+    <p class = "card-text">Jazlyn Fernandez:</p>
       <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt = "Jazlyn" />
     </div>
 
     <div class="card bg-light">
-    <p>Angelina Nantanapramoth: Experience with GUI, Unity, Java, C#, C, and R</p>
+    <p class = "card-text">Angelina Nantanapramoth: Experience with GUI, Unity, Java, C#, C, and R</p>
       <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Angelina" />
     </div>
 
       <div class="card bg-light">
-      <p>Kathareeya Atthajaroon:</p>
+      <p class = "card-text">Kathareeya Atthajaroon:</p>
       <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Kathareeya" />
       </div>
 
       <div class="card bg-light">
-      <p>Daniel Esquivel:</p>
+      <p class = "card-text">Daniel Esquivel:</p>
       <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Daniel" />
       </div>
 
       <div class="card bg-light">
-      <p>Juan Vazquez: Experience with Java, C++, C.</p>
+      <p class = "card-text">Juan Vazquez: Experience with Java, C++, C.</p>
      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Juan" />
       </div>
 
     <div class="card bg-light">
-    <p>Erick Bravo: Experiance with C++, Java, Golang, C, Databases</p>
+    <p class = "card-text">Erick Bravo: Experiance with C++, Java, Golang, C, Databases</p>
     <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="Erick" />
     </div>
 
