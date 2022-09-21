3c501a253cdc4f9823a3f6bb10f0eeda4efa0b45
Garo Demirjian <garabed.demirjian.207@my.csun.edu>
2022-09-21 11:30:39 -0700

    add team members info, and edit homepage

--- pages/caffeine-addicts/index.js
+++ pages/caffeine-addicts/index.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>hi</div>
+  return (
+    <div>
+      <div>
+        <h1>What is the problem being addressed</h1>
+        <p>
+          Currently advising students takes a considerable amount of time and
+          with the explosion of student enrollment it is time consuming for the
+          advisors. This leads to delayed response times for the students and
+          advisors do not have adequate time to give detailed advisement.
+        </p>
+      </div>
+      <div>
+        <h1>
+          What is your project idea and how will it work (what are its
+          components etc.)
+        </h1>
+        <p>
+          We believe there is a big opportunity to alleviate advisor`s time by
+          automating the majority of questions being asked to them. We will be
+          creating a product that can dynamically create a roadmap depending on
+          the student`s needs and preferences (Full time student/preferred
+          discipline, etc). This will give the student exactly what classes they
+          need to take and in what order (Some classes have prerequisites). This
+          will also help with negating user errors and help navigate them
+          through their academic career and ensuring they only take the classes
+          they need. With the support and guidance of multiple faculty members,
+          this can revolutionize academic advisement and give students and
+          advisors a product they can have confidence in.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
