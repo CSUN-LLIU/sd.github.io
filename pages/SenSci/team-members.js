0e2da8fea0ef9e00f2d7e39bc6e6b4d77758fa65
Auraxium <clutchmasterman@gmail.com>
2022-09-21 11:41:27 -0700

    sensci

--- pages/SenSci/team-members.js
+++ pages/SenSci/team-members.js
@@ -1,10 +1,72 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import Image from "next/image"
+
 
 const TeamMembers = () => {
   return (
   <>
-    div.
+    <div class="row g-4">
+                <div class="col-md-6 col-lg-3">
+                    <div class="card bg-light">
+                        <div class="card-body text-center">
+                            <Image src="" width="150px" height="200px" class="mb-3" alt=""/>
+                            <h3 class="card-title mb-3">Lemond Wyatt</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Accusantium praesentium pariatur maxime veritatis doloremque! Doloribus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card bg-light">
+                        <div class="card-body text-center">
+                            <Image src="" width="150px" height="200px"  class="mb-3" alt=""/>
+                            <h3 class="card-title mb-3">Mahima Mishor</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Accusantium praesentium pariatur maxime veritatis doloremque! Doloribus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card bg-light">
+                        <div class="card-body text-center">
+                            <Image src="" width="150px" height="200px"  class="mb-3" alt=""/>
+                            <h3 class="card-title mb-3">Lyana Curry</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Accusantium praesentium pariatur maxime veritatis doloremque! Doloribus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3">
+                    <div class="card bg-light">
+                        <div class="card-body text-center">
+                            <Image src="" width="150px" height="200px"  class="mb-3" alt=""/>
+                            <h3 class="card-title mb-3">Isaias Ramirez</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Accusantium praesentium pariatur maxime veritatis doloremque! Doloribus?
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3">
+                    <div class="card bg-light">
+                        <div class="card-body text-center">
+                            <Image src="" width="150px" height="200px"  class="mb-3" alt=""/>
+                            <h3 class="card-title mb-3">Shashi Wong</h3>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur 
+                                adipisicing elit. Accusantium praesentium pariatur maxime veritatis doloremque! Doloribus?
+                            </p>
+                        </div>
+                    </div>
+                </div>
   </>
   )
 }
