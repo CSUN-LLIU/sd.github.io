143e4b487b625007ac6dcfd5b7776e33557b9f58
sopiloteh <bravoerick9@outlook.com>
2022-09-21 15:09:40 -0700

    update to Team
    Team-memebers.js file updated

--- pages/gryffindor/team-members.js
+++ pages/gryffindor/team-members.js
@@ -1,22 +1,42 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import Image from "next/image"
 
 const TeamMembers = () => {
   return <div>
   <h1>Team Gryffindor</h1>
-  <p>Jazlyn Fernandez:</p>
-    <img src="/sd.github.io-main/public/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" alt = "Jazlyn" />
-  <p>Angelina Nantanapramoth: Experience with GUI, Unity, Java, C#, C, and R</p>
-      <img src="/sd.github.io-main/public/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" alt="Angelina" />
-  <p>Kathareeya Atthajaroon:</p>
-      <img src="/sd.github.io-main/public/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" alt="Kathareeya" />
-  <p>Daniel Esquivel:</p>
-      <img src="/sd.github.io-main/public/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" alt="Daniel" />
-  <p>Juan Vazquez: Experience with Java, C++, C.</p>
-      <img src="/sd.github.io-main/public/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" alt="Juan" />
-  <p>Erick Bravo: Experiance with C++, Java, Golang, C, Databases</p>
-      <img src="/sd.github.io-main/public/gryffindor/panda.jpg" height="150px" width="150px" border="5px" alt="Erick" />
 
-  </div>
+    <div class="card bg-light">
+    <p>Jazlyn Fernandez:</p>
+      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt = "Jazlyn" />
+    </div>
+
+    <div class="card bg-light">
+    <p>Angelina Nantanapramoth: Experience with GUI, Unity, Java, C#, C, and R</p>
+      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Angelina" />
+    </div>
+
+      <div class="card bg-light">
+      <p>Kathareeya Atthajaroon:</p>
+      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Kathareeya" />
+      </div>
+
+      <div class="card bg-light">
+      <p>Daniel Esquivel:</p>
+      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Daniel" />
+      </div>
+
+      <div class="card bg-light">
+      <p>Juan Vazquez: Experience with Java, C++, C.</p>
+     <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Juan" />
+      </div>
+
+    <div class="card bg-light">
+    <p>Erick Bravo: Experiance with C++, Java, Golang, C, Databases</p>
+    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="Erick" />
+    </div>
+
+</div>
 }
 
 export default TeamMembers
