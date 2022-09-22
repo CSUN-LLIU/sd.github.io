75581c5df18c7f6c3e8c4b681f8389050271c53b
sopiloteh <bravoerick9@outlook.com>
2022-09-21 18:43:35 -0700

    updates for Weds - Evening
    Updated teams.js file and public pictures

--- pages/gryffindor/team-members.js
+++ pages/gryffindor/team-members.js
@@ -6,42 +6,75 @@ const TeamMembers = () => {
   return <div>
   <h1>Team Gryffindor</h1>
 
-    <div class="card bg-light">
-    <p>Jazlyn Fernandez:</p>
-      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt = "Jazlyn" />
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+   <Image src="" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <h5 class="card-title">Jazlyn Fernandez</h5>
+      <p class="card-text"> 
+      /*add description here */
+       </p> 
+      
     </div>
+      </div>
+   
 
-    <div class="card bg-light">
-    <p>Angelina Nantanapramoth: Experience with GUI, Unity, Java, C#, C, and R</p>
-      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Angelina" />
+      <div class="col">
+    <div class="card h-100">
+   <Image src="" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <h5 class="card-title">Angelina Nantanapramoth</h5>
+      <p class="card-text"> 
+      Experience with GUI, Unity, Java, C#, C, and R
+       </p> 
+      
     </div>
+      </div>
+      
+    
+    
 
-      <div class="card bg-light">
-      <p>Kathareeya Atthajaroon: </p>
-      <p> Greetings! I am Kathy, it is nice to meet everyone of you.
+      <div class="col">
+    <div class="card h-100">
+   <Image src="/sd.github.io/gryffindorpics/strawberry.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <h5 class="card-title">Kathareeya Atthajaroon</h5>
+      <p class="card-text"> 
+      Greetings! I am Kathy, it is nice to meet everyone of you.
        I have experienced with UI/UX designs, Java/script, C,C++, NODE JS, basics of python, and MYSQL database.
        My current interest in the fields of computer science is data science, and hoping to improve my Python skills.
-       I am willing to learn more skills through new projects that I'm working on.</p>
-      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Kathareeya" />
+       I am willing to learn more skills through new projects that I'm working on.
+       </p> 
+      
+    </div>
+      </div>
+     
+      <div class="col">
+    <div class="card h-100">
+    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <h5 class="card-title">Daniel Esquivel</h5>
+      <p> /*add description here */</p>
+      </div>
       </div>
 
-      <div class="card bg-light">
-      <p>Daniel Esquivel:</p>
-      <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Daniel" />
+      <div class="col">
+    <div class="card h-100">
+    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <h5 class="card-title">Juan Vazquez</h5>
+      <p>Experience with Java, C++, C.</p>
+     
+      </div>
       </div>
 
-      <div class="card bg-light">
-      <p>Juan Vazquez: Experience with Java, C++, C.</p>
-     <Image src="/sd.github.io-main/gryffindor/sloth.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="Juan" />
-      </div>
-
-    <div class="card bg-light">
-    <p>Erick Bravo: Hallo I am Erick, Ill be graduating in Spring 2023 if all goes well<br></br>
+      <div class="col">
+    <div class="card h-100">
+    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <h5 class="card-title">Erick Bravo</h5>
+    <p>Hello I am Erick, Ill be graduating in Spring 2023 if all goes well
         I have the following experiance with C++, Java, Golang, C, Databases. Im currently intrested in imagry and robotics<br></br>
         Always willing to lend a hand with anything and everything as long as we all learn from it.</p>
-    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="Erick" />
+    
     </div>
-
+</div>
+</div>
 </div>
 }
 
