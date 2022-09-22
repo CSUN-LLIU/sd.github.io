87bf9fea0a61546d9dd3d93a8ba54c2f149a4605
Garo Demirjian <garabed.demirjian.207@my.csun.edu>
2022-09-22 00:07:36 -0700

    change css name. modified some pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import NavBar from '../components/nav-bar/NavBar'
 import '../styles/globals.css'
-import '../styles/bit-by-bit/styles.css'
-
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/bit-by-bit/team-members.js
+++ pages/bit-by-bit/team-members.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
+import styles from '../../styles/bit-by-bit/styles.module.css'
 
 const TeamMembers = () => {
-  /*
-  2. Team-Members page.
-    Provide information about EACH member here with image (150 x 200 pixels). 
-    You can add additional information more ABOUT each team member such as interests, specialization, etc.
-  */
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Team Members</h2>
-
       <div>
         <p>
           <Image
             src='/sd.github.io/bit-by-bit-pics/sailor_jupiter_winking_by_noah65478_de019qv-pre.jpg'
             alt='random girl lol'
-            width='200'
-            height='150'
+            width={200}
+            height={150}
           />
           <li>Miranda Medina</li>
           Computer Science major, addicted to coffee and learning new things!
@@ -29,8 +24,8 @@ const TeamMembers = () => {
           <Image
             src='/sd.github.io/bit-by-bit-pics/IMG_0196.JPG'
             alt='Jerry the guinea pig'
-            width='150'
-            height='150'
+            width={150}
+            height={150}
           />
           <li>Lucas Troutman</li>
           Part-time Instructional Aide for a local school, hobby musician, with
@@ -43,8 +38,8 @@ const TeamMembers = () => {
           <Image
             src='/sd.github.io/bit-by-bit-pics/9128029.png'
             alt='not actually an accurate picture'
-            width='150'
-            height='275'
+            width={150}
+            height={275}
           />
           <li>Gino Vitug</li>
           Computer Science major with an interest in UI/UX design. Enjoyer of
@@ -57,8 +52,8 @@ const TeamMembers = () => {
           <Image
             src='/sd.github.io/bit-by-bit-pics/beefa86f25c09019ca1784de1f63a708.jpg'
             alt='bird wearing reindeer antlers'
-            width='200'
-            height='150'
+            width={200}
+            height={150}
           />
           <li>Kathleen Navarro</li>
           Part-time computer science student, part-time web developer for a
@@ -72,8 +67,8 @@ const TeamMembers = () => {
           <Image
             src='/sd.github.io/bit-by-bit-pics/unknown.png'
             alt='rumi'
-            width='150'
-            height='200'
+            width={150}
+            height={200}
           />
           <li>Murssal Rahmati</li>
           Computer Science student with an interest in games, art, and cats.
