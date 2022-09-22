5bdea9e9e18016e5652ae884600dd823f9df3c50
Isaias Ramirez <isaiasramirez@Isaiass-MBP.lan>
2022-09-21 21:38:01 -0700

    sensci

--- pages/SenSci/project-artifacts.js
+++ pages/SenSci/project-artifacts.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const ProjectArtifacts = () => {
-  return <div>ProjectArtifacts</div>
+  return <div>
+    
+    <h1>Project Artifacts</h1>
+    <h2> Prototype Design </h2>
+    <h2>Final Presentation</h2>
+    <h2>Final Project Report</h2>
+    <h2>Posters</h2>
+    <h2>Videos</h2>
+
+  </div>
 }
 
 export default ProjectArtifacts
