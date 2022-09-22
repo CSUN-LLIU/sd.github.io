0ba269c1e16723e366d30ffc576129b60dd3929d
sopiloteh <bravoerick9@outlook.com>
2022-09-21 18:49:33 -0700

    update to index
    changed some paragraphs for placeholders

--- pages/gryffindor/index.js
+++ pages/gryffindor/index.js
@@ -3,6 +3,29 @@ import React from 'react'
 const HomePage = () => {
   return <div>
     <h1>Welcome to Team Gryffindor!</h1>
+    <br></br>
+    <h1>Our Solution To A Changing World</h1>
+    <p>
+      Here at Gryffindor industries, were striving to change the world in inovative ways
+      With the advent of new technologies, we are harnessing this to force a change to improve with our new solutions
+    </p>
+    <h2>The Problem</h2>
+    <p>
+      In our research we have found this troubling problem affecting our community
+    </p>
+    <h2>Our Proposed solution</h2>
+    <p>
+      Here we will have the accomplised this with our prototype solution
+      <br></br>
+      <br></br>
+      * 
+      <br></br>
+      * 
+      <br></br>
+      * 
+      <br></br>
+      * 
+     </p>
     
     </div>
 }
--- pages/gryffindor/project-artifacts.js
+++ pages/gryffindor/project-artifacts.js
@@ -2,28 +2,7 @@ import React from 'react'
 
 const ProjectArtifacts = () => {
   return <div>
-    <h1>Our Solution To A Changing World</h1>
-    <p>
-      Here at Gryffindor industries, we're striving to change the world in inovative ways
-      With the advent of new technologies, we are harnessing this to force a change to improve with our new solutions
-    </p>
-    <h2>The Problem</h2>
-    <p>
-      In our research we have found this troubling problem affecting our community
-    </p>
-    <h2>Our Proposed solution</h2>
-    <p>
-      Here we will have the accomplised this with our prototype solution
-      <br></br>
-      <br></br>
-      * 
-      <br></br>
-      * 
-      <br></br>
-      * 
-      <br></br>
-      * 
-     </p>
+
       <h2>Prototype Evolution</h2>
       <p>
           PLACE HOLDER
