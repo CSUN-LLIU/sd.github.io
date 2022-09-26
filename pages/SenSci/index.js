32579a37c74ab437b9d65088d4fa1e99be08d9d3
Auraxium <clutchmasterman@gmail.com>
2022-09-26 10:59:44 -0700

    added shashi

--- pages/SenSci/index.js
+++ pages/SenSci/index.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import Image from 'next/image'
+
 
 const HomePage = () => {
 
-  let main = {backgroundColor: "#343a40", width: "100%"}
+  let main = { backgroundColor: "#343a40", width: "100%" }
 
   return (
     <>
-    <div className="d-flex flex-column justify-content-center align-items-center">
-      
-      <h1>Parking At CSUN <span className="text-warning">Made Easy</span></h1>
-      <div>No more standing in line for a parking pass</div>
+      <div className="d-flex flex-column justify-content-center align-items-center bg-dark text-light p-5">
+        <h1>Parking At CSUN <span className="text-warning">Made Easy</span></h1>
+        <div className='lead'>No more standing in line for a parking pass</div>
+      </div>
 
-    </div>
+      <section className='bg-primary text-light p-5'>
+        <div class="container">
+          <div class="row align-items-center justify-content-between">
+            <div class="col-md p-5">
+              <h2 class="">Problems</h2>
+              <p class="lead">
+                Parking at CSUN...
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
