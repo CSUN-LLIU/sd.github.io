2e63226c6080cb0c6946934c79991abe68fadc9d
sopiloteh <bravoerick9@outlook.com>
2022-09-26 10:00:56 -0700

    Squashed commit of the following:
    commit dc112ae110d578f31e96ee3da95049d478bcfe85
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Sun Sep 25 16:16:19 2022 -0700
    
        Add caffeine addicts members pictures. Modify team members page
    
    commit a7b5364deafffa19ac46ddd00d2455185d2dad41
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Sun Sep 25 14:08:12 2022 -0700
    
        SenSci add key prop to card
    
    commit 53cba5027d82d24ee3390fac56017d66b43e9a68
    Author: = <=>
    Date:   Fri Sep 23 06:49:25 2022 -0700
    
        asd
    
    commit 01caf9a3dc2e21620097833b2ca76b71e2ff704f
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Thu Sep 22 14:47:42 2022 -0700
    
        fix nav bar
    
    commit cb31f8b370aa1279fec09acb1ba61e1e6c405546
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Thu Sep 22 10:12:36 2022 -0700
    
        fix hydration problem when reloading. bit by bit
    
    commit 865702ef260057bb90c31aaa800a7c7e730c0c48
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Thu Sep 22 00:31:16 2022 -0700
    
        change image type sensci
    
    commit 62b807d63c572d52fc2e7a36ee69a3605fcc4c3d
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Thu Sep 22 00:22:15 2022 -0700
    
        Put width and height in correct format
    
    commit d14ee11577a5f5d12c977c010e505df0f6c40b07
    Author: Isaias Ramirez <isaiasramirez@Isaiass-MBP.lan>
    Date:   Thu Sep 22 00:13:54 2022 -0700
    
        sensci
    
    commit 87bf9fea0a61546d9dd3d93a8ba54c2f149a4605
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Thu Sep 22 00:07:36 2022 -0700
    
        change css name. modified some pages
    
    commit bb33674f3179e9cf81198ccace07e0b4072f974c
    Author: Isaias Ramirez <isaiasramirez@Isaiass-MBP.lan>
    Date:   Wed Sep 21 23:35:59 2022 -0700
    
        sensci
    
    commit 160d3103b62bbd8302def29303cc997fdc23004d
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Wed Sep 21 23:15:55 2022 -0700
    
        modify nav bar css
    
    commit b97a8d47db428a05c6ed9209ed7dee0fca5961fb
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Wed Sep 21 23:01:39 2022 -0700
    
        change project-t image folder
    
    commit 9633ca17a9562ad9f6666ec7a17e56553538fa38
    Merge: 5bdea9e 74528c9
    Author: gdem25 <58720487+gdem25@users.noreply.github.com>
    Date:   Wed Sep 21 22:42:22 2022 -0700
    
        Merge pull request #7 from CSUN-LLIU/group4-bitbybit
    
        Group4 bitbybit
    
    commit 74528c9e392bab9186a2a42185604b15563c6e6a
    Author: Garo Demirjian <garabed.demirjian.207@my.csun.edu>
    Date:   Wed Sep 21 22:41:00 2022 -0700
    
        changed image tag to next Image
    
    commit e428ba025d02528a9d322f7afbb1b2084f67d3a6
    Author: luckytrout <100403421+luckytrout@users.noreply.github.com>
    Date:   Wed Sep 21 18:18:42 2022 -0700
    
        attempt to fix syntax
    
    commit 60522ac28b6db8b7b12ebffefa432b46be82f571
    Author: luckytrout <100403421+luckytrout@users.noreply.github.com>
    Date:   Wed Sep 21 17:49:06 2022 -0700
    
        branch update
    
    commit cd2f345b4e6ccd625c91eaf2ec18df632440977c
    Merge: 5499c96 e039c0b
    Author: luckytrout <100403421+luckytrout@users.noreply.github.com>
    Date:   Wed Sep 21 17:42:56 2022 -0700
    
        Merge branch 'main' of https://github.com/CSUN-LLIU/sd.github.io into group4-bitbybit
    
    commit 5499c968d4d03750f7a194600a71b7b46ab0a038
    Author: luckytrout <100403421+luckytrout@users.noreply.github.com>
    Date:   Wed Sep 21 11:57:05 2022 -0700
    
        fixes to team members page
    
    commit 2f2bc14d8cbe33cc707d15aeba011f397efb492a
    Author: luckytrout <100403421+luckytrout@users.noreply.github.com>
    Date:   Wed Sep 21 11:46:44 2022 -0700
    
        Co-authored-by: Kathleen Navarro <klnavarro@users.noreply.github.com>
        Co-authored-by: Gino Vitug <gino-vi@users.noreply.github.com>
        Co-authored-by: Murs <murssal@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import NavBar from '../components/nav-bar/NavBar'
 import '../styles/globals.css'
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <NavBar>
--- pages/bit-by-bit/index.js
+++ pages/bit-by-bit/index.js
@@ -1,7 +1,65 @@
 import React from 'react'
-
+import Image from 'next/image'
+import styles from '../../styles/bit-by-bit/styles.module.css'
 const HomePage = () => {
-  return <div>hi there hi hi</div>
+  /*
+  1. A homepage
+  This page includes a Project Synopsis which introduces your project. Answer the following questions in separate paragraphs:
+
+  (1) What is the problem being addressed?
+
+  (2) What is your project idea and how will it work (what are its components etc.)
+
+  Use pictures/images to make the page interesting.
+  */
+  return (
+    <div className={styles.container}>
+      <h1>Project: </h1> VR Clinic
+      <p>
+        <Image
+          src='/sd.github.io/bit-by-bit-pics/unknown2.png'
+          alt='Picture from a demo of VR Clinic'
+          width={200}
+          height={200}
+        />
+      </p>
+      <div>
+        <h2>Problem:</h2>
+        <p>
+          Ever since the pandemic hit in 2020, training for real life situations
+          has been difficult and sometimes dangerous. This is true for a number
+          of professions, but nursing deals with close contact between the
+          patient and nurse, which can lead to possible transmission of disease.
+        </p>
+
+        <p>
+          Most student nurses practice examinations on other students. However,
+          most students do not exhibit signs of illness or stress, and so
+          students do not get to learn a wide range of symptoms like they
+          should.
+        </p>
+
+        <p>
+          We are going to solve both of these issues by allowing nurses to train
+          and practice through the use of VR. These simulations will keep
+          students safe by giving them the experience of learning what symptoms
+          are linked to what illnesses, without the risk of catching those
+          illnesses.
+        </p>
+      </div>
+      <div>
+        <h2>Project Idea:</h2>
+        <p>
+          {' '}
+          We are going to continue the Computer Science and Nursing Project from
+          last year. Continuing to add features to the check up simulation, we
+          will need to work with Unity, C#, and also add in 3D modeling and
+          sound effects. The project will be accessible via a VR headset (tested
+          on a Quest 2).
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
