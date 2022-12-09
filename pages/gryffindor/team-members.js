60e5b8e746de27e1c71264d42d51b18e1072cec7
sopiloteh <bravoerick9@outlook.com>
2022-12-08 20:59:02 -0800

    Updates_Dec_8th
    Index, Artifacts, and Team page updates

--- pages/gryffindor/team-members.js
+++ pages/gryffindor/team-members.js
@@ -9,7 +9,7 @@ const TeamMembers = () => {
   <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card h-100">
-   <Image src="" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <Image src="/sd.github.io/gryffindorpics/cinnamoroll.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="" />
    <h5 class="card-title">Jazlyn Fernandez</h5>
       <p class="card-text"> 
       Hello I am Jazlyn, a Senior at CSUN, hoping to graduate in Spring 2023.
@@ -24,12 +24,12 @@ const TeamMembers = () => {
 
       <div class="col">
     <div class="card h-100">
-   <Image src="" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <Image src="/sd.github.io/gryffindorpics/mymelody.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="My Melody" />
    <h5 class="card-title">Angelina Nantanapramoth</h5>
       <p class="card-text"> 
-      I have experience with GUI, Unity, Java, C#, C, and R. 
-      My interest CS is in regards to the development of communication and also game development. 
-      Nevertheless, I am not adverse to trying new things and gaining new skills.
+      {`      I have experience with GUI, Unity, Java, C#, C, and R. My interest CS is in regards to the development of communication and also game development. 
+      Nevertheless, I am not adverse to trying new things and gaining new skills.`}
+
       </p> 
       
     </div>
@@ -40,7 +40,7 @@ const TeamMembers = () => {
 
       <div class="col">
     <div class="card h-100">
-   <Image src="/sd.github.io/gryffindorpics/strawberry.jpg" height="150px" width="150px" border="5px" class="mb-3" alt="" />
+   <Image src="/sd.github.io/gryffindorpics/pochacco.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="Pochacco" />
    <h5 class="card-title">Kathareeya Atthajaroon</h5>
       <p class="card-text"> 
       {`Greetings! I am Kathy, it is nice to meet everyone of you.
@@ -54,29 +54,36 @@ const TeamMembers = () => {
      
       <div class="col">
     <div class="card h-100">
-    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <Image src="/sd.github.io/gryffindorpics/keroppi.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="Keropi" />
     <h5 class="card-title">Daniel Esquivel</h5>
-      <p> Outside of class I enjoy hanging out with friends and learning new skills like drawing. 
-        I have experience using Java, C, and Swift, but I am always willing to learn new languages like Python or C++.</p>
+      <p class="card-text">
+      {`Outside of class I enjoy hanging out with friends and learning new skills like drawing. 
+        I have experience using Java, C, and Swift, but I am always willing to learn new languages like Python or C++.`}  
+        
+</p>
       </div>
       </div>
 
       <div class="col">
     <div class="card h-100">
-    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <Image src="/sd.github.io/gryffindorpics/badtzmaru.jpeg" height="250px" width="250px" border="5px" class="mb-3" alt="Badtz Maru" />
     <h5 class="card-title">Juan Vazquez</h5>
-      <p>Experience with Java, C++, C. Excited to work on any type of software development especially game design.</p>
+      <p class="card-text">
+      {`Experience with Java, C++, C. Excited to work on any type of software development especially game design.`}
+        </p>
      
       </div>
       </div>
 
       <div class="col">
     <div class="card h-100">
-    <Image src="/sd.github.io-main/gryffindor/panda.jpg" height="150px" width="150px"  alt="" />
+    <Image src="/sd.github.io/gryffindorpics/pompom.jpeg" height="250px" width="250px" border="5px" class="mb-3" alt="Badtz Maru" />
     <h5 class="card-title">Erick Bravo</h5>
-    <p>Hello I am Erick, Ill be graduating in Spring 2023 if all goes well
-        I have the following experiance with C++, Java, Golang, C, Databases. Im currently intrested in imagry and robotics<br></br>
-        Always willing to lend a hand with anything and everything as long as we all learn from it.</p>
+    <p class="card-text">
+    {`      Hello I am Erick, Ill be graduating in Spring 2023 if all goes well
+        I have the following experiance with C++, Java, Golang, C, Databases. Im currently intrested in imagry and robotics.
+        Always willing to lend a hand with anything and everything as long as we all learn from it.`}
+</p>
     
     </div>
 </div>
