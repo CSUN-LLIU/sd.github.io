82b36beaeaba2f6cff18ce3c52cd4ff7cfe56da1
luckytrout <100403421+luckytrout@users.noreply.github.com>
2022-12-12 12:21:23 -0800

    added links, text changes, alignment
    I think the text changes are too long but will act as a placeholder for now.

--- pages/bit-by-bit/index.js
+++ pages/bit-by-bit/index.js
@@ -13,49 +13,89 @@ const HomePage = () => {
   Use pictures/images to make the page interesting.
   */
   return (
-    <div className={styles.container}>
-      <h1>Project: </h1> VR Clinic
+    <div className={styles.container} style={{textAlign: `center`}}>
+      <h1>Nursing VR</h1> 
       <p>
-        <Image
+        <Image className={styles.header} style={{textAlign: `center`}}
           src='/sd.github.io/bit-by-bit-pics/unknown2.png'
-          alt='Picture from a demo of VR Clinic'
+          alt='Picture of a simulated patient within Nursing VR'
           width={200}
           height={200}
         />
       </p>
-      <div>
-        <h2>Problem:</h2>
+
+      <h2>Problem</h2>
+      <div style={{textAlign: `justify`}}>
         <p>
           Ever since the pandemic hit in 2020, training for real life situations
-          has been difficult and sometimes dangerous. This is true for a number
-          of professions, but nursing deals with close contact between the
-          patient and nurse, which can lead to possible transmission of disease.
+          has been made especially difficult and has made it more apparent
+          the possible health risks of dealing with those with contagious diseases.
+          This is true for a number of professions but within the medical field,
+          nurses often must be in close contact with their patients for varying lengths of time
+          and both of these factors put their health at risk.
+        </p>
+        <p>
+        Nursing students may not have effective patient subjects to practice their diagnosing techniques and examinations on.
+        The availability, wellness, and consent of a person are some factors to consider when looking for an ideal patient subject.
+        There is a need for nursing students to practice on different areas of the body,
+        like examining the respiratory, cardiovascular, and abdominal systems.
         </p>
 
         <p>
-          Most student nurses practice examinations on other students. However,
-          most students do not exhibit signs of illness or stress, and so
-          students do not get to learn a wide range of symptoms like they
-          should.
+        Assuming their test patients are healthy, Student nurses that practice examining other nursing students
+        do not have the opportunity to practice diagnosing for different types of illnesses or abnormalities that they
+        are taught in their courses or may see in their field. Because healthy/ordinary patients do not exhibit signs 
+        of illness or any abnormalities, the practicing
+        nurses do not get the opportunity to learn or apply what they've learned from a wide range of symptoms.
         </p>
 
         <p>
-          We are going to solve both of these issues by allowing nurses to train
-          and practice through the use of VR. These simulations will keep
-          students safe by giving them the experience of learning what symptoms
-          are linked to what illnesses, without the risk of catching those
-          illnesses.
+        For instructors teaching courses on nursing, test proctoring and giving feedback to students can be very time-consuming
+        and strenuous for both the students and teachers. For example, having to observe head-to-toe assessments for each 
+        individual students can take hours at a time.
+        </p>
+        <p>
+        We are going to solve these issues by allowing nurses to train
+        and practice through the use of VR. These simulations will keep
+        students safe by giving them the experience of learning what symptoms
+        are linked to what illnesses, without the risk of exposing themselves to
+        those illnesses.
+        </p>
+        <p>
+        Because the user will operate in a virtual environment, the product would eliminate any risks and discomfort 
+        that would come with practicing on real people and has the potential to make the training process 
+        more efficient and less time-consuming. Our product would do away with the current practicing methods conducted at CSUN 
+        of having to diagnose and examine a real-life person such as a classmate or any person for that matter, familiar or unfamiliar.
+        </p>
+        <p>
+        Nursing VR may be a useful tool for instructors that teach these nursing courses. The features of this software would help to make student testing and giving student feedback less time-consuming and less strenuous for the instructors.
+        The features of Nursing VR would allow the instructor to customize the simulated patient, with different symptoms and/or abnormalities. Instructors could have more control over what their students can practice diagnosing for and allow for much more freedom which wouldn’t be possible with real-life patients.
+        Because the user will operate in a virtual environment, the product would eliminate any risks and discomfort that would come with practicing on real people and  has the potential to make the training process more efficient and less time-consuming.
+        Our product would do away with the current practicing methods conducted at CSUN of having to diagnose and examine a real-life person such as a classmate or really any person, familiar or unfamiliar.
         </p>
       </div>
-      <div>
-        <h2>Project Idea:</h2>
+
+      <h2>Project Idea</h2>
+      <div style={{textAlign: `justify`}}>  
         <p>
-          {' '}
-          We are going to continue the Computer Science and Nursing Project from
-          last year. Continuing to add features to the check up simulation, we
-          will need to work with Unity, C#, and also add in 3D modeling and
-          sound effects. The project will be accessible via a VR headset (tested
-          on a Quest 2).
+        Nursing VR will provide nursing students a safe and effective way to 
+        practice their diagnosing skills within a virtual environment. 
+        The product allows students to practice medical examinations
+        (provided by CSUN Nursing Department) 
+        which have been implemented into the program. 
+        The user will be able to interact within a 3D virtual space along 
+        with a simulated patient, medical tools, and other assets that nursing 
+        students would have access to in a real-life medical room setting.
+        Ideally, it would also provide an immersive experience that a nursing student 
+        would have when practicing on a live patient. Our project is intended to 
+        run on the Oculus/Meta Quest 2.
+        </p>
+        <p>
+        We will add new features to the existing project, using resources and feedback provided by the
+        CSUN Nursing Department. New features: an Instructor/Administrator Web Interface for
+        customizing the simulated patient and the Cardiovascular System Assessment Exam.
+        For development, we will be applying and using Unity, C#, 3D modeling, and animation.
+        The program runs using a VR device (tested to be compatible with a Meta/Oculus Quest 2).
         </p>
       </div>
     </div>
--- pages/bit-by-bit/project-artifacts.js
+++ pages/bit-by-bit/project-artifacts.js
@@ -17,13 +17,23 @@ const ProjectArtifacts = () => {
     <div className={styles.container}>
       <h1>Project Artifacts</h1>
       <div>
-        <h2>Prototype Design</h2>
+          <h2>Prototype Design</h2>
       </div>
       <div>
         <h2>Final Presentation</h2>
+        <u>
+          <a href='https://docs.google.com/presentation/d/1BuEe8iQ_Ywgp9cOHZ5KoI-HhuYO0zJxLnIjdoz38bnM/edit?usp=sharing'>
+          Senior Project Presentation (Fall 2022)
+          </a>
+        </u>
       </div>
       <div>
         <h2>Final Project Report</h2>
+        <u>
+          <a href='https://docs.google.com/document/d/1qJDFJC3khcYYVn2EBiwrGAlK04vukSo7/edit?usp=sharing&ouid=100717398472660853610&rtpof=true&sd=true'>
+          Senior Design Project Document (Fall 2022)
+          </a>
+        </u>
       </div>
       <div>
         <h2> Posters </h2>
