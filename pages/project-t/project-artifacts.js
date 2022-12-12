33d93cd85176eb4a58c3c83e3183968b160b2009
sari-ajina <sari.ajina@hotmail.com>
2022-12-12 10:34:34 -0800

    adjusted code added css

--- pages/project-t/project-artifacts.js
+++ pages/project-t/project-artifacts.js
@@ -3,19 +3,21 @@ import Image from 'next/image'
 import styles from '../../styles/projectTCSS/styles.module.css'
 
 const ProjectArtifacts = () => {
-  return <div >
+  return <div className={styles.container}>
     <h1>Project Artifacts </h1> 
     <h2>Prototype Design</h2> 
       <Image
         src='/sd.github.io/project-t-pics/ERD.png'
         alt='ERD pic'
-        width={150}
-        height={200}
+        width= {500}
+        height={500}
       />
     <h2>Final Presentation</h2> 
       <p>
-        <a href="https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16">First Presentation</a>
-        <a href="https://docs.google.com/presentation/d/1PCAXZ6ytd8xfqXpC95_jV9_V3EZLn2LinT2YlrKz0X8/edit">Second Presentation</a>
+        <a href="https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16" style={{color: `blue`}}>First Presentation</a>
+      </p>
+      <p>
+        <a href="https://docs.google.com/presentation/d/1PCAXZ6ytd8xfqXpC95_jV9_V3EZLn2LinT2YlrKz0X8/edit" style={{color: `blue`}}>Second Presentation</a>
       </p>
     <h2>Final Project Report</h2> 
     <h2>Posters</h2> 
