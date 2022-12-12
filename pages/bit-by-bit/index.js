ca69a634c0b9f25f9d6c46dc8e070ff17bbd8804
luckytrout <100403421+luckytrout@users.noreply.github.com>
2022-12-12 15:40:10 -0800

    shortened problem statement

--- pages/bit-by-bit/index.js
+++ pages/bit-by-bit/index.js
@@ -2,18 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/bit-by-bit/styles.module.css'
 const HomePage = () => {
-  /*
-  1. A homepage
-  This page includes a Project Synopsis which introduces your project. Answer the following questions in separate paragraphs:
-
-  (1) What is the problem being addressed?
-
-  (2) What is your project idea and how will it work (what are its components etc.)
-
-  Use pictures/images to make the page interesting.
-  */
   return (
-    <div className={styles.container} style={{textAlign: `center`}}>
+    <div className={styles.container} style={{textAlign: `center`}} >
       <h1>Nursing VR</h1> 
       <p>
         <Image className={styles.header} style={{textAlign: `center`}}
@@ -23,58 +13,20 @@ const HomePage = () => {
           height={200}
         />
       </p>
-
+      <br></br>
       <h2>Problem</h2>
       <div style={{textAlign: `justify`}}>
         <p>
-          Ever since the pandemic hit in 2020, training for real life situations
-          has been made especially difficult and has made it more apparent
-          the possible health risks of dealing with those with contagious diseases.
-          This is true for a number of professions but within the medical field,
-          nurses often must be in close contact with their patients for varying lengths of time
-          and both of these factors put their health at risk.
-        </p>
-        <p>
-        Nursing students may not have effective patient subjects to practice their diagnosing techniques and examinations on.
-        The availability, wellness, and consent of a person are some factors to consider when looking for an ideal patient subject.
-        There is a need for nursing students to practice on different areas of the body,
-        like examining the respiratory, cardiovascular, and abdominal systems.
-        </p>
-
-        <p>
-        Assuming their test patients are healthy, Student nurses that practice examining other nursing students
-        do not have the opportunity to practice diagnosing for different types of illnesses or abnormalities that they
-        are taught in their courses or may see in their field. Because healthy/ordinary patients do not exhibit signs 
-        of illness or any abnormalities, the practicing
-        nurses do not get the opportunity to learn or apply what they've learned from a wide range of symptoms.
-        </p>
-
-        <p>
-        For instructors teaching courses on nursing, test proctoring and giving feedback to students can be very time-consuming
-        and strenuous for both the students and teachers. For example, having to observe head-to-toe assessments for each 
-        individual students can take hours at a time.
-        </p>
-        <p>
-        We are going to solve these issues by allowing nurses to train
-        and practice through the use of VR. These simulations will keep
-        students safe by giving them the experience of learning what symptoms
-        are linked to what illnesses, without the risk of exposing themselves to
-        those illnesses.
-        </p>
-        <p>
-        Because the user will operate in a virtual environment, the product would eliminate any risks and discomfort 
-        that would come with practicing on real people and has the potential to make the training process 
-        more efficient and less time-consuming. Our product would do away with the current practicing methods conducted at CSUN 
-        of having to diagnose and examine a real-life person such as a classmate or any person for that matter, familiar or unfamiliar.
-        </p>
-        <p>
-        Nursing VR may be a useful tool for instructors that teach these nursing courses. The features of this software would help to make student testing and giving student feedback less time-consuming and less strenuous for the instructors.
-        The features of Nursing VR would allow the instructor to customize the simulated patient, with different symptoms and/or abnormalities. Instructors could have more control over what their students can practice diagnosing for and allow for much more freedom which wouldn’t be possible with real-life patients.
-        Because the user will operate in a virtual environment, the product would eliminate any risks and discomfort that would come with practicing on real people and  has the potential to make the training process more efficient and less time-consuming.
-        Our product would do away with the current practicing methods conducted at CSUN of having to diagnose and examine a real-life person such as a classmate or really any person, familiar or unfamiliar.
+        There are nursing students who do not have effective patient subjects to practice
+        their diagnosing techniques and their assessments on. The availability, wellness, consent, and diversity
+        of a person are some factors to consider when looking for an ideal patient subject.
+        There are health risks to consider when physically assessing others in such close proximity.
+        For instructors teaching courses on nursing, test proctoring and giving feedback 
+        to students can be very time-consuming and strenuous for both the students and teachers.
         </p>
       </div>
 
+      <br></br>
       <h2>Project Idea</h2>
       <div style={{textAlign: `justify`}}>  
         <p>
--- pages/bit-by-bit/project-artifacts.js
+++ pages/bit-by-bit/project-artifacts.js
@@ -14,12 +14,15 @@ const ProjectArtifacts = () => {
   */
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{textAlign: `center`}}>
+      <br></br>
       <h1>Project Artifacts</h1>
       <div>
           <h2>Prototype Design</h2>
       </div>
+
       <div>
+        <br></br>
         <h2>Final Presentation</h2>
         <u>
           <a href='https://docs.google.com/presentation/d/1BuEe8iQ_Ywgp9cOHZ5KoI-HhuYO0zJxLnIjdoz38bnM/edit?usp=sharing'>
@@ -27,7 +30,9 @@ const ProjectArtifacts = () => {
           </a>
         </u>
       </div>
+
       <div>
+        <br></br>
         <h2>Final Project Report</h2>
         <u>
           <a href='https://docs.google.com/document/d/1qJDFJC3khcYYVn2EBiwrGAlK04vukSo7/edit?usp=sharing&ouid=100717398472660853610&rtpof=true&sd=true'>
@@ -35,14 +40,19 @@ const ProjectArtifacts = () => {
           </a>
         </u>
       </div>
+
       <div>
+      <br></br>
         <h2> Posters </h2>
       </div>
+      
       <div>
+      <br></br>
         <h2>Videos</h2>
       </div>
     </div>
-  )
+  ) 
+
 }
 
 export default ProjectArtifacts
