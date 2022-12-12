7a781f33aa375fb1f50d8072fc8e7f2c0a363e91
sari-ajina <sari.ajina@hotmail.com>
2022-12-12 10:51:36 -0800

    added caption

--- pages/project-t/project-artifacts.js
+++ pages/project-t/project-artifacts.js
@@ -13,6 +13,9 @@ const ProjectArtifacts = () => {
         height={650}
         className={styles.header}
       />
+      <p> 
+        The &#39;User&#39; entity contains the preliminary information that the user will specify such as age, sex, and the selected body part. The REST API then narrows down the probing analysis questions and symptoms based on the user&#39;s input provided from. This process repeats until enough information about the patient&#39;s issue is gathered. Then, analysis from the user&#39;s answers leads to the &#39;Results&#39; entity with further diagnosis information and resources towards care. 
+      </p>
     <h2>Final Presentation</h2> 
       <p>
         <a href="https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16" style={{color: `blue`}}>First Presentation</a>
