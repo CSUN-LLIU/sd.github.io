4f49b8d46a119f267b72a97a70fd0291c8958eff
Garo Demirjian <garabed.demirjian.207@my.csun.edu>
2022-12-11 18:25:19 -0800

    Add Artifacts

--- pages/gryffindor/team-members.js
+++ pages/gryffindor/team-members.js
@@ -1,94 +1,128 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import Image from "next/image"
+import Image from 'next/image'
 
 const TeamMembers = () => {
-  return <div>
-  <h1>Team Gryffindor</h1>
+  return (
+    <div>
+      <h1>Team Gryffindor</h1>
 
-  <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-   <Image src="/sd.github.io/gryffindorpics/cinnamoroll.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="" />
-   <h5 class="card-title">Jazlyn Fernandez</h5>
-      <p class="card-text"> 
-      Hello I am Jazlyn, a Senior at CSUN, hoping to graduate in Spring 2023.
-       I have experience in mostly Java and some experience with C, C++. 
-       My interest in computer science is game developing and GUI design. But I am always open to learn more and improve my skills 
-       for this field.
-       </p> 
-      
-    </div>
-      </div>
-   
+      <div className='row row-cols-1 row-cols-md-3 g-4'>
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/cinnamoroll.jpg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt=''
+            />
+            <h5 className='card-title'>Jazlyn Fernandez</h5>
+            <p className='card-text'>
+              Hello I am Jazlyn, a Senior at CSUN, hoping to graduate in Spring
+              2023. I have experience in mostly Java and some experience with C,
+              C++. My interest in computer science is game developing and GUI
+              design. But I am always open to learn more and improve my skills
+              for this field.
+            </p>
+          </div>
+        </div>
 
-      <div class="col">
-    <div class="card h-100">
-   <Image src="/sd.github.io/gryffindorpics/mymelody.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="My Melody" />
-   <h5 class="card-title">Angelina Nantanapramoth</h5>
-      <p class="card-text"> 
-      {`      I have experience with GUI, Unity, Java, C#, C, and R. My interest CS is in regards to the development of communication and also game development. 
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/mymelody.jpg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt='My Melody'
+            />
+            <h5 className='card-title'>Angelina Nantanapramoth</h5>
+            <p className='card-text'>
+              {`      I have experience with GUI, Unity, Java, C#, C, and R. My interest CS is in regards to the development of communication and also game development. 
       Nevertheless, I am not adverse to trying new things and gaining new skills.`}
+            </p>
+          </div>
+        </div>
 
-      </p> 
-      
-    </div>
-      </div>
-      
-    
-    
-
-      <div class="col">
-    <div class="card h-100">
-   <Image src="/sd.github.io/gryffindorpics/pochacco.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="Pochacco" />
-   <h5 class="card-title">Kathareeya Atthajaroon</h5>
-      <p class="card-text"> 
-      {`Greetings! I am Kathy, it is nice to meet everyone of you.
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/pochacco.jpg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt='Pochacco'
+            />
+            <h5 className='card-title'>Kathareeya Atthajaroon</h5>
+            <p className='card-text'>
+              {`Greetings! I am Kathy, it is nice to meet everyone of you.
        I have experienced with UI/UX designs, Java/script, C,C++, NODE JS, basics of python, and MYSQL database.
        My current interest in the fields of computer science is data science, and hoping to improve my Python skills.
        I am willing to learn more skills through new projects that I'm working on.`}
-       </p> 
-      
-    </div>
-      </div>
-     
-      <div class="col">
-    <div class="card h-100">
-    <Image src="/sd.github.io/gryffindorpics/keroppi.jpg" height="250px" width="250px" border="5px" class="mb-3" alt="Keropi" />
-    <h5 class="card-title">Daniel Esquivel</h5>
-      <p class="card-text">
-      {`Outside of class I enjoy hanging out with friends and learning new skills like drawing. 
-        I have experience using Java, C, and Swift, but I am always willing to learn new languages like Python or C++.`}  
-        
-</p>
-      </div>
-      </div>
+            </p>
+          </div>
+        </div>
 
-      <div class="col">
-    <div class="card h-100">
-    <Image src="/sd.github.io/gryffindorpics/badtzmaru.jpeg" height="250px" width="250px" border="5px" class="mb-3" alt="Badtz Maru" />
-    <h5 class="card-title">Juan Vazquez</h5>
-      <p class="card-text">
-      {`Experience with Java, C++, C. Excited to work on any type of software development especially game design.`}
-        </p>
-     
-      </div>
-      </div>
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/keroppi.jpg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt='Keropi'
+            />
+            <h5 className='card-title'>Daniel Esquivel</h5>
+            <p className='card-text'>
+              {`Outside of className I enjoy hanging out with friends and learning new skills like drawing. 
+        I have experience using Java, C, and Swift, but I am always willing to learn new languages like Python or C++.`}
+            </p>
+          </div>
+        </div>
 
-      <div class="col">
-    <div class="card h-100">
-    <Image src="/sd.github.io/gryffindorpics/pompom.jpeg" height="250px" width="250px" border="5px" class="mb-3" alt="Badtz Maru" />
-    <h5 class="card-title">Erick Bravo</h5>
-    <p class="card-text">
-    {`      Hello I am Erick, Ill be graduating in Spring 2023 if all goes well
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/badtzmaru.jpeg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt='Badtz Maru'
+            />
+            <h5 className='card-title'>Juan Vazquez</h5>
+            <p className='card-text'>
+              {`Experience with Java, C++, C. Excited to work on any type of software development especially game design.`}
+            </p>
+          </div>
+        </div>
+
+        <div className='col'>
+          <div className='card h-100'>
+            <Image
+              src='/sd.github.io/gryffindorpics/pompom.jpeg'
+              height='250px'
+              width='250px'
+              border='5px'
+              className='mb-3'
+              alt='Badtz Maru'
+            />
+            <h5 className='card-title'>Erick Bravo</h5>
+            <p className='card-text'>
+              {`      Hello I am Erick, Ill be graduating in Spring 2023 if all goes well
         I have the following experiance with C++, Java, Golang, C, Databases. Im currently intrested in imagry and robotics.
         Always willing to lend a hand with anything and everything as long as we all learn from it.`}
-</p>
-    
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</div>
-</div>
+  )
 }
 
 export default TeamMembers
