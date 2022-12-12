b184093c7f79de2338904a291a672acd78e6bc49
sari-ajina <sari.ajina@hotmail.com>
2022-12-12 10:20:21 -0800

    testing hyperlink

--- pages/project-t/project-artifacts.js
+++ pages/project-t/project-artifacts.js
@@ -5,6 +5,7 @@ const ProjectArtifacts = () => {
     <h1>Project Artifacts </h1> 
     <p>Prototype Design</p> 
     <p>Final Presentation</p> 
+      <a href="https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16">First Presentation</a>
     <p>Final Project Report</p> 
     <p>Posters</p> 
     <p>Videos</p> 
