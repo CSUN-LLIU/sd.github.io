156e5a3ca6e00c8077f27337ebe71ab14cdc7f4d
AngelinaNantanapramoth <fahangelina168@gmail.com>
2023-05-08 11:58:30 -0700

    Updated  Venus contents

--- pages/gryffindor/index.js
+++ pages/gryffindor/index.js
@@ -22,31 +22,29 @@ const HomePage = () => {
       
     </h1>
     <p>
-      Here at Gryffindor industries, were striving to change the world in inovative ways
-      With the advent of new technologies, we are harnessing this to force a change to improve with our new solutions<br></br><br></br>
       
-      <i>&emsp;&emsp;Machine learning algorithims, Interactive procedural built worlds, downloadable content, VR gaming, movie quality storylines<br></br>
-      &emsp;&emsp;Ray tracing, Cloud gaming, The advent of massively online multiplayer, both player vs enviroment and player vs player & enviroment</i><br></br><br></br>
-      These are just some of the latest technologies & improvements a person can find in the latest AAA game. All these technologies can provide a fascinatingly amazing game<br></br> 
-      experiance which can lead to new insights into experiancing life. However all of these implementations can lead to some confusion, a tsunami of information.<br></br>
-      This often times can instead of creating wonder, will cause discord with the player base.<br></br>
+      
+      When playing a video game, there's often a sense of thrill when players are out exploring in the digital frontier. However there are moments in games where <br></br>
+      one is often left confused on what to do. Usually these confused moments are met with hints and guides to get a player out of that situation. That is, if it’s <br></br>
+      added to the game. New video games such as Scorn offer visually fantastic games but they leave the player wondering what exactly is going on. There is no built-in sense<br></br>
+      of what to do and if the player does manage to figure something out, they wind up frustrated at it having taken so long. What we aim to do is create a virtual <br></br>
+      helper that will calm down those moments of confused panic. The helper, Venus won’t play the game but will be a vigilant sentinel ready at a moments notice to <br></br>
+      leap in and see what the player needs help with gamewise. <br></br>
     </p>
     <h2>The Problem</h2>
     <p>
-    &emsp;In our research we have found this troubling problem affecting our community. Often times, a new game will come up with no tutorial or have a confusing<br></br> 
-      way to play that can infuriate the player. Enraged the player will then rage in an onine forum or demand a refund claiming the game has broken mechanics.<br></br>
-      The problem is exacerbated by the decline of offical game guides which were prevalent from the advent of video games until the early 2010s and <br></br> 
-      the advent of streaming via Twitch or YouTube where an expecation is set when a person is watching who is intrested in a new game.<br></br>
-      A player then wants to get that same experiance but with no recording, article or other information and often times little patience, would then <br></br>
-      loose it in the heat of battle. These experiances can be troubling and dissaude game develepers from wanting to create new intricate games.<br></br>
+    &emsp;Too often games come out with complex tasks that seem obscure to the player. Our Venus Hint system, assisted by machine learning techniques, <br></br>
+    is specifically designed to alleviate gamer’s frustrations caused by the complexity of the puzzle game. Our goal is to provide assistance to gamers <br></br>
+    during gameplay, while also respecting their preferences by allowing them to disable the hint system. We want Venus to give value to the player by allowing them<br></br>
+    a way to play the game without unnecessary frustration. Venus will help with that undue stress. This will give a sense of satisfaction when the player completes the game.<br></br>
 
     </p>
     <h2>Thinking outside the box</h2>
     
     <p>
-    &emsp;Our proposed solution for this problem is to create an AI helper called Venus that can learn the players play-style and situations where the player is having difficulty.<br></br>
-    The proof of concept would take this AI helper and apply it to a confusingly fun browser based game called <strong><a herf="https://the-house.arturkot.pl/"> The House </a></strong>by Arthur Kot<br></br>
-    As the game is browser based we will have to implement a way to capture the users gameplay then send this back to the users game state to allow the new solution to play out.<br></br>
+    &emsp;Our proposed solution for this problem is to create an AI helper called Venus that can anticipate the player's frsutration and read their current progress.<br></br>
+    The proof of concept would take this AI helper and apply it to a confusing browser based game called <strong><a herf="https://the-house.arturkot.pl/"> The House </a></strong>by Arthur Kot<br></br>
+    As the game is browser based we will have to implement a way to a way to analyze player's moveents, game state, and generate hints.<br></br>
     
     </p>
 
