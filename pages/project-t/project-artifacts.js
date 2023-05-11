0e261148bfac787291d6cdc175fe77bfa375fe5e
Garo Demirjian <garabed.demirjian.207@my.csun.edu>
2023-05-11 01:27:23 -0700

    more fixes

--- pages/project-t/project-artifacts.js
+++ pages/project-t/project-artifacts.js
@@ -4,40 +4,58 @@ import styles from '../../styles/projectTCSS/styles.module.css'
 
 const ProjectArtifacts = () => {
   return (
-  <div className={styles.container}>
-    <h1>Project Artifacts </h1> 
-    <h2>Prototype Design</h2> 
+    <div className={styles.container}>
+      <h1>Project Artifacts </h1>
+      <h2>Prototype Design</h2>
       <Image
         src='/sd.github.io/project-t-pics/ERD.png'
         alt='ERD pic'
-        width= {650}
+        width={650}
         height={650}
         className={styles.header}
       />
-      <p className={styles.paragraph}> 
-        The &#39;User&#39; entity contains the preliminary information that the user will specify such as age, sex, and the selected body part. The REST API then narrows down the probing analysis questions and symptoms based on the user&#39;s input provided from. This process repeats until enough information about the patient&#39;s issue is gathered. Then, analysis from the user&#39;s answers leads to the &#39;Results&#39; entity with further diagnosis information and resources towards care. 
+      <p className={styles.paragraph}>
+        The &#39;User&#39; entity contains the preliminary information that the
+        user will specify such as age, sex, and the selected body part. The REST
+        API then narrows down the probing analysis questions and symptoms based
+        on the user&#39;s input provided from. This process repeats until enough
+        information about the patient&#39;s issue is gathered. Then, analysis
+        from the user&#39;s answers leads to the &#39;Results&#39; entity with
+        further diagnosis information and resources towards care.
       </p>
-    <h2>Final Presentation</h2> 
+      <h2>Final Presentation</h2>
       <p>
-        <a target='_blank' href="https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16" style={{color: `blue`}}>First Presentation</a>
+        <a
+          href='https://docs.google.com/presentation/d/1GYLood-k2MBgN4XAr0ZsffVnG-FZhX3AQwGh5KS39as/edit#slide=id.gd9c453428_0_16'
+          style={{ color: `blue` }}>
+          First Presentation
+        </a>
       </p>
       <p>
-        <a target='_blank' href="https://docs.google.com/presentation/d/1PCAXZ6ytd8xfqXpC95_jV9_V3EZLn2LinT2YlrKz0X8/edit" style={{color: `blue`}}>Second Presentation</a>
+        <a
+          href='https://docs.google.com/presentation/d/1PCAXZ6ytd8xfqXpC95_jV9_V3EZLn2LinT2YlrKz0X8/edit'
+          style={{ color: `blue` }}>
+          Second Presentation
+        </a>
       </p>
       <p>
-        <a target='_blank' href="https://docs.google.com/presentation/d/1StotcjyET8o7qdhg5VccdIhHNkIZ7cvXKxx0uCuNn9s/edit" style={{color: `blue`}}>Final Presentation</a>
+        <a
+          href='https://docs.google.com/presentation/d/1StotcjyET8o7qdhg5VccdIhHNkIZ7cvXKxx0uCuNn9s/edit'
+          style={{ color: `blue` }}>
+          Final Presentation
+        </a>
       </p>
-    <h2>Final Project Report</h2> 
-    <h2>Posters</h2> 
-    <Image 
-      src='/sd.github.io/project-t-pics/swiftcheckposter.png'
-      alt='swiftcheck poster'
-      width={1000}
-      height={650}
-    />
-    <h2>Videos</h2> 
-    <a target='_blank' href="https://www.youtube.com/watch?v=Aw6rP-cRC-0">Demo Video!</a>
-  </div>
+      <h2>Final Project Report</h2>
+      <h2>Posters</h2>
+      <Image
+        src='/sd.github.io/project-t-pics/swiftcheckposter.png'
+        alt='swiftcheck poster'
+        width={1000}
+        height={650}
+      />
+      <h2>Videos</h2>
+      <a href='https://www.youtube.com/watch?v=Aw6rP-cRC-0'>Demo Video!</a>
+    </div>
   )
 }
 
